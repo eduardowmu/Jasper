5003fd11106180f1bb50c66daa014ee243b808c8
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-02-06 11:24:23 -0300

    Relatorio com nova lib no eclipse

--- demo-jasper-jdbc-2/src/main/java/com/mballem/jasper/jdbc/App.java
+++ demo-jasper-jdbc-2/src/main/java/com/mballem/jasper/jdbc/App.java
@@ -9,14 +9,14 @@ import com.mballem.jasper.jdbc.service.JasperService;
 /*https://stackoverflow.com/questions/20991641/jasper-compiler-error-when-using-sum-method*/
 public class App 
 {	public static void main( String[] args ) throws SQLException
-    {abrirJrxml("09");}
+    {abrirJrxml("18");}
 
 	private static void abrirJrxml(String numero) throws SQLException
 	{	Connection connection = JdbcConnection.connection();
 		JasperService service = new JasperService();
 		/*método inserido previamente na classe de serviço (JasperService)*/
-		service.addParams("NIVEL", 1);
-		service.addParams("SALARY", new BigDecimal(10));
+		//service.addParams("NIVEL", 1);
+		//service.addParams("SALARY", new BigDecimal(10));
 		service.abrirJasperViewer("src/main/resources/relatorios/Funcionarios-" + numero + ".jrxml", connection);
 		connection.close();
 	}
