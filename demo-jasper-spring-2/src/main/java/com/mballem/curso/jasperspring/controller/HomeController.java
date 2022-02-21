e3e00844f35a1430a3da5889c5cac6b789c1d87d
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2022-02-20 21:22:04 -0300

    projeto final

--- demo-jasper-spring-2/src/main/java/com/mballem/curso/jasperspring/controller/HomeController.java
+++ demo-jasper-spring-2/src/main/java/com/mballem/curso/jasperspring/controller/HomeController.java
@@ -6,10 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mballem.curso.jasperspring.entity.Funcionario;
+import com.mballem.curso.jasperspring.repository.FuncionarioRepository;
 
 @Controller
 public class HomeController 
 {	@Autowired Connection connection;
+	@Autowired FuncionarioRepository repository;
 	
 	@GetMapping("/")
     public String index() {return "index";}
@@ -19,6 +24,14 @@ public class HomeController
 	@GetMapping("/conn")
     public String myConnection(Model model) 
 	{	model.addAttribute("conn", connection != null ? "Connection worked" : "Connection failt");
+		return "index";
+    }
+	
+	@GetMapping("/certificados")
+    public String certificadoValidador(@RequestParam("cid")Long cid, Model model) 
+	{	Funcionario funcionario = this.repository.findById(cid).get();
+		model.addAttribute("mensagem", "Confirmamos a veracidade deste certificado, pertencente "
+				+ "a " + funcionario.getNome() + " emitido em " + funcionario.getDataDemissao());
 		return "index";
     }
 }
